113a44e0ab128d495357d7ce2d2d8bfaf838aa8a
raulguiii <raul.rodrigues@aluno.faculdadeimpacta.com.br>
2025-04-03 23:46:54 -0300

    entrar e sair funcionando

---
+++ static/js/login.js
@@ -0,0 +1,4 @@
+document.getElementById("loginForm").addEventListener("submit", function (event) {
+    event.preventDefault(); // Impede o envio padrão do formulário
+    window.location.href = "index.html"; // Redireciona para index.html
+});
