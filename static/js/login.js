f5bb3da44366440a9d015786cf83658d6dd66fa1
raulguiii <raul.rodrigues@aluno.faculdadeimpacta.com.br>
2025-04-14 19:44:07 -0300

    validando login

--- static/js/login.js
+++ static/js/login.js
@@ -1,4 +1,9 @@
 document.getElementById("loginForm").addEventListener("submit", function (event) {
     event.preventDefault(); // Impede o envio padrão do formulário
-    window.location.href = "index.html"; // Redireciona para index.html
+
+    // Simula um login válido
+    localStorage.setItem("isLoggedIn", "true");
+
+    // Redireciona para a página principal
+    window.location.href = "index.html";
 });
