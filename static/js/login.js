613e302c71f2d103af9efd3fb3b1de6556eace31
raulguiii <raul.rodrigues@aluno.faculdadeimpacta.com.br>
2025-04-15 09:08:01 -0300

    expiração login 15 min

--- static/js/login.js
+++ static/js/login.js
@@ -1,8 +1,9 @@
 document.getElementById("loginForm").addEventListener("submit", function (event) {
     event.preventDefault(); // Impede o envio padrão do formulário
 
-    // Simula um login válido
+    // Salva login e horário atual (em milissegundos)
     localStorage.setItem("isLoggedIn", "true");
+    localStorage.setItem("loginTime", Date.now());
 
     // Redireciona para a página principal
     window.location.href = "index.html";
